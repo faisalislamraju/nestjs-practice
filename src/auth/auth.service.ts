0501de85d90a619ab6079ad6adab2a831dbd8154
Faisal Islam <faisal.islam@monstar-lab.com>
2022-05-20 02:36:11 +0600

    added validation and user creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import { User, Bookmark} from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dtos";
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
+    constructor(private prisma: PrismaService) {}
     signin() {
         return  {msg: 'I am signed in'};
     }
 
-    signup() {
-        return { msg: 'I am signed up'};
+    async signup(dto: AuthDto) {
+        //generate password hash
+        const hash = await argon.hash(dto.password);
+        //save the new user credentials
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            }
+        });
+
+        //return saved user
+        return user;
     }
 }
